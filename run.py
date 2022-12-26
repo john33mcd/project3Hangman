f6586a2c94b9f7f97316982fe78c5e602b8dac00
john33mcd <jmcd-34@hotmail.com>
2022-12-26 17:52:59 +0000

    added functionality and validation for accepting name of user

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
 import random #import random in order to use randomize function for words
 words = ['sandwich', 'random', 'secret', 'formulate', 'vintage', 'python', 'implicate', 'congratulate']
-random.shuffle(words)
-print(words)
+
 def nameRequest():
-    name = input("Hi, please enter your name:")
-    print(f"Hi {name}, time to save a life !")
+    '''
+    requests the users name and starts the game once a name is provided, will only accept letters
+    '''
+    name = ''
+    while True:
+        name = input("Hi, please enter your name:")
+        if not name.isalpha():
+            print("please enter a name with letters only!")
+            continue
+        else:
+            print(f"Hi {name}, time to save a life !")
+            break
 #def hangman():
 nameRequest()
