ac0ab27e5de2afdb9c763ef92f10599bea780985
john33mcd <jmcd-34@hotmail.com>
2022-12-27 13:57:49 +0000

    added lives and function to track lives, responses for each input from the user also added

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import random #import random in order to use randomize function for words
 #all words using uppercase to match expected input below
 words = ['SANDWICH', 'RANDOM', 'SECRET', 'FORMULATE', 'VINTAGE', 'PYTHON', 'IMPLICATE', 'CONGRATULATE']
 import string #imported to verify letters against the alphabet
+lives = 5
 def get_word(words):
     '''
     get random word from pre populated list and apply a random function on it, return 
@@ -27,17 +28,20 @@ def name_request():
 def guess_letter():
     '''
     function to allow user to make a guess, converts any guess to uppercase and 
-    only allows alphabet entry. the majority of the below code block has been taken
-    from the youtube tutorial from Kylie Ying, link and credits will be included 
-    in readMe file
+    only allows alphabet entry. the guess letter function also tracks lives
+    and responds to each input by the user.
+    the majority of the below code block has been taken from the youtube tutorial from Kylie Ying, 
+    link and credits will be included in readMe file
     '''
     word = get_word(words)
     word_letters = set(word) #letters in the word
     alphabet = set(string.ascii_uppercase)#checks predetermined alphabet
     used_letters = set() #what the user has input
-    while len(word_letters) > 0: #while there is still letters to be filled
+    lives = 7
+    while len(word_letters) > 0 and lives > 0: #while there is still letters to be filled
         #letters that have been used
         print('letters used so far: ', ' '.join(used_letters)) #gives string of used letters
+        print(f'you have {lives} lives left')
         word_list = [letter if letter in used_letters else '_' for letter in word]
         print('Current word:', ' '.join(word_list))
         
@@ -46,13 +50,21 @@ def guess_letter():
             used_letters.add(guess)
             if guess in word_letters:
                 word_letters.remove(guess)
+                print('well done, that was a correct guess')
+
+            else:
+                lives = lives - 1
+                print(f"hard luck, you have lost a life, only {lives} left")
             
         elif guess in used_letters:
             print('you have already guessed this letter, try again')
         else:
             print('the character you have entered has not been recognized, please try again')
-    # print(f"you have guessed {guess} !")
-
+    
+    if lives == 0:
+        print(f'oh no, you have ran out of lives, better luck next time !\n the word was {word}')
+    else:
+        print(f'congratulations, you won, thank you for playing \n you successfully guessed {word}')
 
 def hangman():
     get_word(words)
